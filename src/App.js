17f62c30be6f3269afa5f21f350388b008dbfbbb
chenzexi <chenzexi@imdada.cn>
2023-02-06 17:19:21 +0800

    style(text):测试ci

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ppxppxppxppxppxppxppxppxppxppxppxppxppxppx
+          7777777777777
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ppxppxppxppxppxppxppxppxppxppxppxppxppxppxppx
+          7777777777777
         </a>
       </header>
     </div>
